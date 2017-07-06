7321867f854888b97d628641ae949f8185df1a2c
Tim <timossenblok@gmail.com>
2017-07-06 17:15:18 +0200

    Create jumpseat_post.php

---
+++ core/templates/jumpseat_post.php
@@ -0,0 +1,15 @@
+<div class="content">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="box box-primary">
+		        <div class="box-header with-border">
+		            <h3 class="box-title">Jumpseat</h3>
+		        </div>
+		        <div class="box-body">
+		            <p>Pilot (ID: <?php echo $pilot; ?>) has requested a jumpseat ticket from <?php echo $from; ?> to <?php echo $dest; ?> - Cost: <?php echo $price; ?></p>
+		            <p>Processing....</p>
+		        </div>
+		    </div>
+		</div>
+	</div>
+</div>
