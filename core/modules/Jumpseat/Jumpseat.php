c4c2f2b8919da21d7997b661c71244ee7bbb646e
Tim <timossenblok@gmail.com>
2017-07-06 15:56:32 +0200

    Create Jumpseat.php

---
+++ core/modules/Jumpseat/Jumpseat.php
@@ -0,0 +1,42 @@
+<?php
+
+	class Jumpseat extends CodonModule
+	{
+
+
+		public function index()
+		{	
+
+			$search = array('p.pilotid' => Auth::$userinfo->pilotid,'p.accepted' => PIREP_ACCEPTED);
+      $jumpseatdep = (!$reports) ? Auth::$userinfo->hub : $reports->arricao;
+
+			$this->set('jumpseatdep','test');
+			$this->render('jumpseat.php');
+		}
+
+
+
+		public function fly()
+		{
+			if (isset($this->post->action)) {
+            switch ($this->post->action) {
+                case 'fly':
+
+                    $this->processJump();
+                    $this->set('dest',$this->post->jumpseatdest);
+                    $this->set('from',$this->post->departfrom);
+                    $this->render('jumpseat_post.php');
+                    break;
+            	}
+        	}
+			
+		}
+
+		protected function processJump()
+		{	
+			// process location change
+		}
+
+	}
+
+?>
